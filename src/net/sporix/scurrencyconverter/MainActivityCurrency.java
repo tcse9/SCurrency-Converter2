875a3bab67b94805b1c91d1b060a893481bd9aae
taufiq_ovi <ovioviovi@google.com>
2013-06-23 02:26:06 +0600

    First commit(test commit)
    Signed-off-by: taufiq_ovi <ovioviovi@google.com>

---
+++ src/net/sporix/scurrencyconverter/MainActivityCurrency.java
@@ -0,0 +1,24 @@
+package net.sporix.scurrencyconverter;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.support.v4.app.NavUtils;
+
+public class MainActivityCurrency extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.layout_main, menu);
+        return true;
+    }
+
+    
+}
